98bf5a49de6f916309f6ad92b377c6eafb69d7c9
Rizalie Belarmino <102493219+rizbelarmino@users.noreply.github.com>
2024-05-23 17:44:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from sklearn.metrics import classification_report
 def app():
     
     st.title('Symbol Classification')
-    st.subheader('by Louie F. Cervantes M.Eng., WVSU College of ICT')
+    st.subheader('by Rizalie S. Belarmino, WVSU College of ICT')
     
     st.write('Dataset description:')
 
